2adee4e082988e707ed53fa3fdd3a2ca1575a085
Bruno Quintella <bquinte@Allianzire.ie>
2018-01-16 16:19:27 +0000

    Testing flexbox

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from '../shared/material.module';
 
@@ -13,6 +14,7 @@ import { FlexboxComponent } from './flexbox/flexbox.component';
     CommonModule,
     FormsModule,
     MaterialModule,
+    FlexLayoutModule,
     DemoRoutingModule
   ],
   declarations: [ButtonsComponent, FlexboxComponent]
